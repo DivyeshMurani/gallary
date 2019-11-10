07c6368b98f9eabd044f5c929e397ea17ee79202
divyesh <m.divvu@gmail.com>
2019-11-10 22:51:52 +0530

    init

--- app/src/main/java/com/oscandev/gallery/adapter/FolderLoadAdapter.java
+++ app/src/main/java/com/oscandev/gallery/adapter/FolderLoadAdapter.java
@@ -29,13 +29,20 @@ public class FolderLoadAdapter extends BaseRecyclerViewAdapter {
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, final int i) {
-        FolderViewHolder  holder= (FolderViewHolder) viewHolder;
+        FolderViewHolder holder = (FolderViewHolder) viewHolder;
         holder.txt_folder_name.setText(list.get(i));
+
+        if (list.size() - 1 == i) {
+            holder.view_line.setVisibility(View.GONE);
+        }else {
+            holder.view_line.setVisibility(View.VISIBLE);
+        }
+
         holder.root.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (clcik!=null) {
-                    clcik.addOnClick(i,list.get(i));
+                if (clcik != null) {
+                    clcik.addOnClick(i, list.get(i));
                 }
             }
         });
@@ -49,13 +56,17 @@ public class FolderLoadAdapter extends BaseRecyclerViewAdapter {
     public class FolderViewHolder extends RecyclerView.ViewHolder {
         private TextView txt_folder_name;
         private LinearLayout root;
+        private View view_line;
 
-        public FolderViewHolder(@NonNull View itemView) {
+        /**
+         * @param itemView
+         */
+
+        FolderViewHolder(@NonNull View itemView) {
             super(itemView);
             txt_folder_name = itemView.findViewById(R.id.txt_folder_name);
-            root= itemView.findViewById(R.id.root);
+            root = itemView.findViewById(R.id.root);
+            view_line = itemView.findViewById(R.id.view_line);
         }
     }
-
-
 }
